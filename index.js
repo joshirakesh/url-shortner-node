4e2843b01e000a9d9d2e8ff55797541fd46ef4b4
Rakesh Joshi <rakesh.joshi@meetingdecisions.com>
2023-07-24 01:28:32 +0530

    add route to get actual web redirect when using shorten url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const urlRoute = require('./routes/url');
+const url = require('./models/url');
 const { connectToMongoDB } = require('./connect');
 
 const app = express();
@@ -12,6 +13,18 @@ connectToMongoDB('mongodb://127.0.0.1:27017/url-shortener-node').then(() => {
 app.use(express.json());
 
 app.use('/url', urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await url.findOneAndUpdate({ shortId }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now()
+            }
+        }
+    }, { new: true });
+    res.redirect(entry?.redirectUrl);
+});
 
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
 
