7c3851907e644f8a75655491919febbfa3f0e537
Rakesh joshi <irakeshjoshi@live.com>
2023-07-24 23:59:27 +0530

    moved up

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const urlRoute = require('./routes/url');
 const { handleGetRedirectUrlFromShortId } = require('./controllers/url');
 
 const { connectToMongoDB } = require('./connect');
+const urlDbName = 'url-shortener-node';
 
 const app = express();
 const PORT = 8001;
 const { connectionStr } = require('./env');
 
-const urlDbName = 'url-shortener-node';
 connectToMongoDB(connectionStr + '/' + urlDbName).then(() => {
     console.log('Connected to MongoDB');
 });
